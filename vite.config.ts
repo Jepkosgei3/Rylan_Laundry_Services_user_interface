073227354fb2b857238f938618ba86ea19f29aca
Jepkosgei3 <cosydeveloper@gmail.com>
2025-02-08 20:12:09 +0300

    updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
     port: 5173, // For local development
   },
   preview: {
-    port: 8081, // For production preview
+    port: 8080, // For production preview
   },
-};
+  });
